f2a24fe7df61dabaab544a94f4e17b5e378cd38f
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2025-03-02 00:22:12 +0530

    first commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -43,7 +43,7 @@ export function HeroSection() {
 						showGradient={true}
 					/>
 				</div>
-				<div className="h-screen flex justify-center items-center flex-col relative p-4">
+				<div className="md:hidden h-screen flex justify-center items-center flex-col relative p-4">
 					<motion.h1
 						initial={{
 							opacity: 0,
@@ -57,14 +57,14 @@ export function HeroSection() {
 							duration: 0.5,
 							ease: [0.4, 0.0, 0.2, 1],
 						}}
-						className="md:hidden text-2xl px-4 md:text-4xl lg:text-7xl font-bold text-neutral-700 dark:text-white max-w-5xl leading-8 lg:leading-snug text-center mx-auto "
+						className="md:hidden text-2xl px-4 md:text-4xl lg:text-7xl font-bold text-neutral-700 dark:text-white max-w-5xl leading-8 lg:leading-snug text-center mx-auto"
 					>
 						Transform <Highlight>Feedback</Highlight> into Your
 						Channel’s Secret Weapon
 					</motion.h1>
 					<Safari
 						url="magicui.design"
-						className="max-w-md w-full h-fit mt-6 shadow"
+						className="max-w-md w-full h-fit mt-6 shadow md:hidden"
 						imageSrc="/heroImage.jpeg"
 					/>
 				</div>
@@ -75,8 +75,6 @@ export function HeroSection() {
 				ease={80}
 				color={color}
 				refresh={true}
-                vx={0.5}
-                vy={0.5}
 			/>
 		</div>
 	);
